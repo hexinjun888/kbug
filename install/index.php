047ee02903901b5bbb9a7b03a7e2fc4ab09ecb17
hexinjun <hexinjun888@163.com>
2018-02-26 18:10:31 +0800

    1.完成install功能

--- install/index.php
+++ install/index.php
@@ -35,7 +35,8 @@ if(@$_GET['c']=='success')
         $link=@new mysqli("{$data['DB_HOST']}:{$data['DB_PORT']}",$data['DB_USER'],$data['DB_PWD']);
         // 获取错误信息
         $error=$link->connect_error;
-        if (!is_null($error)) {
+        if (!is_null($error)) 
+		{
             // 转义防止和alert中的引号冲突
             $error=addslashes($error);
             die("<script>alert('数据库链接失败:$error');history.go(-1)</script>");
@@ -44,7 +45,8 @@ if(@$_GET['c']=='success')
         $link->query("SET NAMES 'utf8'");
         $link->server_info>5.0 or die("<script>alert('请将您的mysql升级到5.0以上');history.go(-1)</script>");
         // 创建数据库并选中
-        if(!$link->select_db($data['DB_NAME'])){
+        if(!$link->select_db($data['DB_NAME']))
+		{
             $create_sql='CREATE DATABASE IF NOT EXISTS '.$data['DB_NAME'].' DEFAULT CHARACTER SET utf8;';
             $link->query($create_sql) or die('创建数据库失败');
             $link->select_db($data['DB_NAME']);
@@ -52,28 +54,62 @@ if(@$_GET['c']=='success')
         // 导入sql数据并创建表
         $bjyadmin_str=file_get_contents('./bjyadmin.sql');
         $sql_array=preg_split("/;[\r\n]+/", str_replace('bjyadmin_',$data['DB_PREFIX'],$bjyadmin_str));
-        foreach ($sql_array as $k => $v) {
-            if (!empty($v)) {
+        foreach ($sql_array as $k => $v) 
+		{
+            if (!empty($v)) 
+			{
                 $link->query($v);
             }
         }
         $link->close();
         $db_str=<<<php
 <?php
-return array(
+return [
+    // 数据库类型
+    'type'            => 'mysql',
+    // 服务器地址
+    'hostname'        => '{$data['DB_HOST']}',
+    // 数据库名
+    'database'        => '{$data['DB_NAME']}',
+    // 用户名
+    'username'        => '{$data['DB_USER']}',
+    // 密码
+    'password'        => '{$data['DB_PWD']}',
+    // 端口
+    'hostport'        => '{$data['DB_PORT']}',
+    // 连接dsn
+    'dsn'             => '',
+    // 数据库连接参数
+    'params'          => [],
+    // 数据库编码默认采用utf8
+    'charset'         => 'utf8',
+    // 数据库表前缀
+    'prefix'          => '{$data['DB_PREFIX']}',
+    // 数据库调试模式
+    'debug'           => true,
+    // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+    'deploy'          => 0,
+    // 数据库读写是否分离 主从式有效
+    'rw_separate'     => false,
+    // 读写分离后 主服务器数量
+    'master_num'      => 1,
+    // 指定从服务器序号
+    'slave_no'        => '',
+    // 是否严格检查字段是否存在
+    'fields_strict'   => true,
+    // 数据集返回类型
+    'resultset_type'  => 'array',
+    // 自动写入时间戳字段
+    'auto_timestamp'  => false,
+    // 时间字段取出后的默认时间格式
+    'datetime_format' => 'Y-m-d H:i:s',
+    // 是否需要进行SQL性能分析
+    'sql_explain'     => false,
+];
 
-//*************************************数据库设置*************************************
-    'DB_TYPE'               =>  'mysqli',                 // 数据库类型
-    'DB_HOST'               =>  '{$data['DB_HOST']}',     // 服务器地址
-    'DB_NAME'               =>  '{$data['DB_NAME']}',     // 数据库名
-    'DB_USER'               =>  '{$data['DB_USER']}',     // 用户名
-    'DB_PWD'                =>  '{$data['DB_PWD']}',      // 密码
-    'DB_PORT'               =>  '{$data['DB_PORT']}',     // 端口
-    'DB_PREFIX'             =>  '{$data['DB_PREFIX']}',   // 数据库表前缀
-);
 php;
         // 创建数据库链接配置文件
-        file_put_contents('../../Application/Common/Conf/db.php', $db_str);
+        file_put_contents('../kbug/database.php', $db_str);
         @touch('./install.lock');
         require './success.html';
     }
